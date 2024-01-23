5bec4657b5ec9feff5a352d823a761b5461256a6
maramar0414 <maramar0414@gmail.com>
2024-01-23 10:36:26 +0000

    form styling started

--- blog-starter-app/components/feedback.tsx
+++ blog-starter-app/components/feedback.tsx
@@ -28,13 +28,16 @@ const Feedback = () => {
 }
     return (
     <>
-    <form action="/" onSubmit={submitForm}>
+    <form 
+    action="/" onSubmit={submitForm}>
+    <div >
     <h1>Send me your thoughts </h1>
-    <textarea placeholder="Message..." rows={10} ref={messageRef} >
+    <textarea className="border-black border-3" placeholder="Message..." rows={10} ref={messageRef} >
        </textarea>
        <button type="submit" className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"> 
-       Send Feedback in the branch
+       Send Feedback
        </button>
+       </div>
        </form> 
       </>
     )
