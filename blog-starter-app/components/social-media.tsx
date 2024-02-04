ae4810c7535da4126a67a6727b7d835c9216a24e
maramar0414 <maramar0414@gmail.com>
2024-02-04 19:53:49 +0000

    social-icons margin

--- blog-starter-app/components/social-media.tsx
+++ blog-starter-app/components/social-media.tsx
@@ -1,6 +1,6 @@
 const SocialMedia = () => {
     return (
-        <div className= "flex flex-col lg:flex-row items-center gap-5" >
+        <div className= "flex flex-col lg:flex-row items-center gap-5 mx-20[']" >
          <img  className="hover:animate-spin" src="https://img.icons8.com/ios/50/github--v1.png" alt="github--v1"/>
          <img className="hover:animate-spin" src="https://img.icons8.com/ios/50/linkedin-circled--v1.png" alt="linkedin-circled--v1"/>
          <img className="hover:animate-spin" src="https://img.icons8.com/ios/50/email.png" alt="email"/>
